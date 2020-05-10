9abd24f4e531c30e8583e74fa6e39aa4783a0443
rubycheen <rubychen@ntu.im>
2020-05-10 18:53:22 +0800

    [Add Tables] add 7 tables

--- SAD_final/FengCramSchool/admin.py
+++ SAD_final/FengCramSchool/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from .models import StudentInfo, Attendance
+from .models import StudentInfo, Attendance, SchoolRecord,CramRecord, Quiz, CourseSchedule, Tuition, Scholarship, Video
 
 # Register your models here.
 admin.site.register(StudentInfo)
 admin.site.register(Attendance)
+admin.site.register(SchoolRecord)
+admin.site.register(CramRecord)
+admin.site.register(Quiz)
+admin.site.register(CourseSchedule)
+admin.site.register(Tuition)
+admin.site.register(Scholarship)
+admin.site.register(Video)
 
 
--- SAD_final/FengCramSchool/models.py
+++ SAD_final/FengCramSchool/models.py
@@ -26,3 +26,66 @@ class Attendance(models.Model):
     def __str__(self):
         return self.SID
         
+class SchoolRecord(models.Model):
+    SID = models.CharField(max_length = 20)
+    category = models.CharField(max_length = 20)
+    subject = models.CharField(max_length = 20) #可改成選項
+    record_url = models.URLField
+    grade = models.CharField(max_length = 20)
+    scholarshipID = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.SID
+
+class CramRecord(models.Model):
+    SID = models.CharField(max_length = 20)
+    quizID = models.CharField(max_length = 20)
+    quiz_grade = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.SID
+
+class Quiz(models.Model):
+    quizID = models.CharField(max_length = 20)
+    quiz_date = models.DateField()
+    quiz_description = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.quizID
+
+class CourseSchedule(models.Model):
+    courseID = models.CharField(max_length = 20)
+    course = models.CharField(max_length = 20)
+    class_time = models.TimeField
+
+    def __str__(self):
+        return self.courseID
+
+
+class Tuition(models.Model):
+    SID = models.CharField(max_length = 20)
+    received_date = models.DateField
+    courseID = models.CharField(max_length = 20)
+    tuition_payment = models.CharField(max_length = 10)
+
+    def __str__(self):
+        return self.SID
+
+class Scholarship(models.Model):
+    SID = models.CharField(max_length = 20)
+    scholarshipID = models.CharField(max_length = 20)
+    payment_date = models.DateField
+    scholarship_description = models.CharField(max_length = 20)
+    scholarship_payment = models.CharField(max_length = 10)
+
+    def __str__(self):
+        return self.SID
+
+class Video(models.Model):
+    VID = models.CharField(max_length = 20)
+    courseID = models.CharField(max_length = 20)
+    video_url = models.URLField
+    course_date = models.DateField
+    
+    def __str__(self):
+        return self.VID
