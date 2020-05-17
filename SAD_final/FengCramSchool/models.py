17c677e58ac59d9d65c708b5d95a855438175bda
corylus98 <yuchen_chen@hotmail.com>
2020-05-17 13:18:04 +0800

    Allow blank when adding new data/Allow NULL in database

--- SAD_final/FengCramSchool/models.py
+++ SAD_final/FengCramSchool/models.py
@@ -5,58 +5,58 @@ from django.db import models
 class StudentInfo(models.Model):
     SID = models.CharField(max_length = 20)
     student_name = models.CharField(max_length = 10)
-    birthday = models.DateField()
-    school = models.CharField(max_length = 20)
-    year = models.DecimalField(max_digits=1, decimal_places=0)
-    address = models.CharField(max_length = 50)
-    student_phone = models.CharField(max_length = 20)
-    parent_name = models.CharField(max_length = 10)
-    parent_phone = models.CharField(max_length = 20)
-    sibling_name = models.CharField(max_length = 10)
-    email = models.CharField(max_length = 30)
+    birthday = models.DateField(blank=True, null = True)
+    school = models.CharField(max_length = 20, blank=True, null = True)
+    year = models.DecimalField(max_digits=1, decimal_places=0, blank=True, null = True)
+    address = models.CharField(max_length = 50, blank=True, null = True)
+    student_phone = models.CharField(max_length = 20, blank=True, null = True)
+    parent_name = models.CharField(max_length = 10, blank=True, null = True)
+    parent_phone = models.CharField(max_length = 20, blank=True, null = True)
+    sibling_name = models.CharField(max_length = 10, blank=True, null = True)
+    email = models.CharField(max_length = 30, blank=True, null = True)
 
     def __str__(self):
         return self.student_name
 
 class Attendance(models.Model):
     SID = models.CharField(max_length = 20)
-    date_time = models.DateTimeField()
-    in_out = models.BooleanField()
+    date_time = models.DateTimeField(blank=True, null = True)
+    in_out = models.NullBooleanField(blank=True, null = True)
 
     def __str__(self):
         return self.SID
         
 class SchoolRecord(models.Model):
     SID = models.CharField(max_length = 20)
-    category = models.CharField(max_length = 20)
-    subject = models.CharField(max_length = 20) #可改成選項
-    record_url = models.URLField()
-    grade = models.CharField(max_length = 20)
-    scholarshipID = models.CharField(max_length = 20)
+    category = models.CharField(max_length = 20, blank=True, null = True)
+    subject = models.CharField(max_length = 20, blank=True, null = True) #可改成選項
+    record_url = models.URLField(blank=True, null = True)
+    grade = models.CharField(max_length = 20, blank=True, null = True)
+    scholarshipID = models.CharField(max_length = 20, blank=True, null = True)
 
     def __str__(self):
         return self.SID
 
 class CramRecord(models.Model):
     SID = models.CharField(max_length = 20)
-    quizID = models.CharField(max_length = 20)
-    quiz_grade = models.CharField(max_length = 20)
+    quizID = models.CharField(max_length = 20, blank=True, null = True)
+    quiz_grade = models.CharField(max_length = 20, blank=True, null = True)
 
     def __str__(self):
         return self.SID
 
 class Quiz(models.Model):
     quizID = models.CharField(max_length = 20)
-    quiz_date = models.DateField()
-    quiz_description = models.CharField(max_length = 20)
+    quiz_date = models.DateField(blank=True, null = True)
+    quiz_description = models.CharField(max_length = 20, blank=True, null = True)
 
     def __str__(self):
         return self.quizID
 
 class CourseSchedule(models.Model):
     courseID = models.CharField(max_length = 20)
-    course = models.CharField(max_length = 20)
-    class_time = models.TimeField()
+    course = models.CharField(max_length = 20, blank=True, null = True)
+    class_time = models.TimeField(blank=True, null = True)
 
     def __str__(self):
         return self.courseID
@@ -64,28 +64,28 @@ class CourseSchedule(models.Model):
 
 class Tuition(models.Model):
     SID = models.CharField(max_length = 20)
-    received_date = models.DateField()
-    courseID = models.CharField(max_length = 20)
-    tuition_payment = models.CharField(max_length = 10)
+    received_date = models.DateField(blank=True, null = True)
+    courseID = models.CharField(max_length = 20, blank=True, null = True)
+    tuition_payment = models.CharField(max_length = 10, blank=True, null = True)
 
     def __str__(self):
         return self.SID
 
 class Scholarship(models.Model):
     SID = models.CharField(max_length = 20)
-    scholarshipID = models.CharField(max_length = 20)
-    payment_date = models.DateField()
-    scholarship_description = models.CharField(max_length = 20)
-    scholarship_payment = models.CharField(max_length = 10)
+    scholarshipID = models.CharField(max_length = 20, blank=True, null = True)
+    payment_date = models.DateField(blank=True, null = True)
+    scholarship_description = models.CharField(max_length = 20, blank=True, null = True)
+    scholarship_payment = models.CharField(max_length = 10, blank=True, null = True)
 
     def __str__(self):
         return self.SID
 
 class Video(models.Model):
     VID = models.CharField(max_length = 20)
-    courseID = models.CharField(max_length = 20)
-    video_url = models.URLField()
-    course_date = models.DateField()
+    courseID = models.CharField(max_length = 20, blank=True, null = True)
+    video_url = models.URLField(blank=True, null = True)
+    course_date = models.DateField(blank=True, null = True)
     
     def __str__(self):
         return self.VID
