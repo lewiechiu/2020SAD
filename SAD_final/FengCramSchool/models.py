4a70dd1ae98a19f9df87582d56a2c649e0ed8fd7
corylus98 <yuchen_chen@hotmail.com>
2020-06-02 11:02:21 +0800

    models.py add Chinese field names and table names

--- SAD_final/FengCramSchool/models.py
+++ SAD_final/FengCramSchool/models.py
@@ -3,22 +3,23 @@ from django.db import models
 # Create your models here.
 
 class StudentInfo(models.Model):
-    SID = models.CharField(max_length = 20)
-    student_name = models.CharField(max_length = 30)
-    birthday = models.DateField(blank=True, null = True)
-    school = models.CharField(max_length = 50, blank=True, null = True)
-    year = models.DecimalField(max_digits=2, decimal_places=0, blank=True, null = True)
-    address = models.CharField(max_length = 100, blank=True, null = True)
-    student_phone = models.CharField(max_length = 20, blank=True, null = True)
-    parent_name = models.CharField(max_length = 30, blank=True, null = True)
-    parent_phone = models.CharField(max_length = 20, blank=True, null = True)
-    sibling_name = models.CharField(max_length = 30, blank=True, null = True)
-    email = models.CharField(max_length = 50, blank=True, null = True)
+    SID = models.CharField('學生編號', max_length = 20)
+    student_name = models.CharField('學生姓名', max_length = 30)
+    birthday = models.DateField('生日', blank=True, null = True)
+    school = models.CharField('學校', max_length = 50, blank=True, null = True)
+    year = models.DecimalField('年級', max_digits=2, decimal_places=0, blank=True, null = True)
+    address = models.CharField('地址', max_length = 100, blank=True, null = True)
+    student_phone = models.CharField('學生電話', max_length = 20, blank=True, null = True)
+    parent_name = models.CharField('家長姓名', max_length = 30, blank=True, null = True)
+    parent_phone = models.CharField('家長電話', max_length = 20, blank=True, null = True)
+    sibling_name = models.CharField('兄弟姐妹姓名', max_length = 30, blank=True, null = True)
+    email = models.CharField('電子郵件', max_length = 50, blank=True, null = True)
 
     def __str__(self):
         return self.student_name
     
     class Meta:
+        verbose_name = '學生資料'
         verbose_name_plural = '學生資料'
 
 class Attendance(models.Model):
@@ -29,16 +30,20 @@ class Attendance(models.Model):
         on_delete = models.CASCADE,
         default = 1
     )
-    date_time = models.DateTimeField(blank=True, null = True)
+    date_time = models.DateTimeField('時間', blank=True, null = True)
     IN_OUT_CHOICES = (
-        ('in', 'IN'),
-        ('out', 'OUT')
+        ('in', '進班'),
+        ('out', '出班')
     )
     # in_out = models.NullBooleanField(blank=True, null = True)
-    in_out = models.CharField(max_length = 5, choices = IN_OUT_CHOICES, default = 'in', blank=True, null = True)
+    in_out = models.CharField('進班/出班', max_length = 5, choices = IN_OUT_CHOICES, default = 'in', blank=True, null = True)
 
     def __str__(self):
         return self.student_info.student_name + '_' + str(self.date_time)
+
+    class Meta:
+        verbose_name = '出缺席紀錄'
+        verbose_name_plural = '出缺席紀錄'
         
 class SchoolRecord(models.Model):
     # SID = models.CharField(max_length = 20)
@@ -48,27 +53,35 @@ class SchoolRecord(models.Model):
         on_delete = models.CASCADE,
         default = 1
     )
-    category = models.CharField(max_length = 50, blank=True, null = True)
+    category = models.CharField('考試類別', max_length = 50, blank=True, null = True)
     SUBJECT_CHOICES = (
-        ('math', 'Math'),
-        ('english', 'English'),
-        ('chinese', 'Chinese')
+        ('math', '數學'),
+        ('english', '英文'),
+        ('chinese', '國文')
     )
-    subject = models.CharField(max_length = 20, choices = SUBJECT_CHOICES, default= 'math', blank=True, null = True)
-    record_url = models.URLField(blank=True, null = True)
-    grade = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null = True)
-    scholarshipID = models.CharField(max_length = 20, blank=True, null = True)
+    subject = models.CharField('科目', max_length = 20, choices = SUBJECT_CHOICES, default= 'math', blank=True, null = True)
+    record_url = models.URLField('成績單連結', blank=True, null = True)
+    grade = models.DecimalField('分數', max_digits=4, decimal_places=1, blank=True, null = True)
+    scholarshipID = models.CharField('獎學金編號', max_length = 20, blank=True, null = True)
 
     def __str__(self):
         return self.student_info.student_name + '_' + self.scholarshipID
 
+    class Meta:
+        verbose_name = '在校成績紀錄'
+        verbose_name_plural = '在校成績紀錄'
+
 class Quiz(models.Model):
-    quizID = models.CharField(max_length = 20)
-    quiz_date = models.DateField(blank=True, null = True)
-    quiz_description = models.CharField(max_length = 100, blank=True, null = True)
+    quizID = models.CharField('小考編號', max_length = 20)
+    quiz_date = models.DateField('小考日期', blank=True, null = True)
+    quiz_description = models.CharField('小考內容', max_length = 100, blank=True, null = True)
 
     def __str__(self):
         return self.quizID
+
+    class Meta:
+        verbose_name = '補習班小考資訊'
+        verbose_name_plural = '補習班小考資訊'
 
 class CramRecord(models.Model):
     # SID = models.CharField(max_length = 20)
@@ -85,19 +98,26 @@ class CramRecord(models.Model):
         on_delete = models.CASCADE,
         default = 1
     )
-    quiz_grade = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null = True)
+    quiz_grade = models.DecimalField('小考成績', max_digits=4, decimal_places=1, blank=True, null = True)
 
     def __str__(self):
         return self.student_info.student_name + '_' + self.quiz.quizID
 
+    class Meta:
+        verbose_name = '補習班小考成績紀錄'
+        verbose_name_plural = '補習班小考成績紀錄'
+
 class CourseSchedule(models.Model):
-    courseID = models.CharField(max_length = 20)
-    course = models.CharField(max_length = 50, blank=True, null = True)
-    class_time = models.TimeField(blank=True, null = True)
+    courseID = models.CharField('課程編號', max_length = 20)
+    course = models.CharField('課程名稱', max_length = 50, blank=True, null = True)
+    class_time = models.TimeField('課程時間', blank=True, null = True)
 
     def __str__(self):
         return self.courseID + '_' + self.course
 
+    class Meta:
+        verbose_name = '課程表'
+        verbose_name_plural = '課程表'
 
 class Tuition(models.Model):
     # SID = models.CharField(max_length = 20)
@@ -107,7 +127,7 @@ class Tuition(models.Model):
         on_delete = models.CASCADE,
         default = 1
     )
-    received_date = models.DateField(blank=True, null = True)
+    received_date = models.DateField('收款日期', blank=True, null = True)
     # courseID = models.CharField(max_length = 20, blank=True, null = True)
     course_schedule = models.ForeignKey(
         CourseSchedule,
@@ -115,10 +135,14 @@ class Tuition(models.Model):
         on_delete = models.CASCADE,
         default = 1
     )
-    tuition_payment = models.DecimalField(max_digits=6, decimal_places=1, blank=True, null = True)
+    tuition_payment = models.DecimalField('收款金額', max_digits=6, decimal_places=1, blank=True, null = True)
     
     def __str__(self):
         return self.student_info.student_name + '_' + self.course_schedule.courseID
+
+    class Meta:
+        verbose_name = '學生繳費記錄'
+        verbose_name_plural = '學生繳費記錄'
 
 class Scholarship(models.Model):
     # SID = models.CharField(max_length = 20)
@@ -135,15 +159,19 @@ class Scholarship(models.Model):
         default = 1
     )
     # scholarshipID = models.CharField(max_length = 20, blank = True, null = True)
-    payment_date = models.DateField(blank=True, null = True)
-    scholarship_description = models.CharField(max_length = 100, blank=True, null = True)
-    scholarship_payment = models.DecimalField(max_digits=6, decimal_places=1, blank=True, null = True)
+    payment_date = models.DateField('頒發日期', blank=True, null = True)
+    scholarship_description = models.CharField('頒發理由', max_length = 100, blank=True, null = True)
+    scholarship_payment = models.DecimalField('獎學金金額', max_digits=6, decimal_places=1, blank=True, null = True)
 
     def __str__(self):
         return self.student_info.student_name + '_' + self.school_record.scholarshipID
 
+    class Meta:
+        verbose_name = '獎學金紀錄'
+        verbose_name_plural = '獎學金紀錄'
+
 class Video(models.Model):
-    VID = models.CharField(max_length = 20)
+    VID = models.CharField('課程影片編號', max_length = 20)
     # courseID = models.CharField(max_length = 20, blank=True, null = True)
     course_schedule = models.ForeignKey(
         CourseSchedule,
@@ -151,8 +179,12 @@ class Video(models.Model):
         on_delete = models.CASCADE,
         default = 1
     )
-    video_url = models.URLField(blank=True, null = True)
-    course_date = models.DateField(blank=True, null = True)
+    video_url = models.URLField('課程影片連結', blank=True, null = True)
+    course_date = models.DateField('課程日期', blank=True, null = True)
     
     def __str__(self):
         return self.VID
+
+    class Meta:
+        verbose_name = '課程影片連結'
+        verbose_name_plural = '課程影片連結'
