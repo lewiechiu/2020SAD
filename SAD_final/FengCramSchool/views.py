ce124cfcc1c0c0745aceb9f2c1768769480388ea
Safero <41570263+Safero@users.noreply.github.com>
2020-06-04 16:43:44 +0800

    Revert "Revert "Homepage update (send email temporarily disabled)""
    This reverts commit 0d95579f3d4e54d61899af1505f1a7478e3184e3.

--- SAD_final/FengCramSchool/views.py
+++ SAD_final/FengCramSchool/views.py
@@ -31,80 +31,84 @@ def homepage(request):
     return render(request, 'home.html')
 
 def send_video_URL(request):
-    # select當天video的資料
-    video_ = list(Video.objects.filter(
-        course_date=datetime.date(2020, 5, 17)).values())  # 這邊應該要是today
+    if request.method == "GET":
+        # select當天video的資料
+        video_ = list(Video.objects.filter(
+            course_date=datetime.date(2020, 5, 17)).values())  # 這邊應該要是today
 
-    # courseID X URL dictionary  這門課的影片連結
-    CID_URL = {}
-    for i in range(len(video_)):
-        if video_[i]["course_schedule_id"] not in CID_URL.keys():
-            CID_URL[video_[i]["course_schedule_id"]] = video_[i]["video_url"]
+        # courseID X URL dictionary  這門課的影片連結
+        CID_URL = {}
+        for i in range(len(video_)):
+            if video_[i]["course_schedule_id"] not in CID_URL.keys():
+                CID_URL[video_[i]["course_schedule_id"]] = video_[i]["video_url"]
 
-    # courseID X SID dictionary  上這門課的學生有哪些
-    CID_SID = {}
-    for i in CID_URL.keys():
-        CID_SID[i] = []
-        cids_sid = list(Tuition.objects.filter(course_schedule_id=i).values())
-        for j in range(len(cids_sid)):
-            CID_SID[i].append(cids_sid[j]["student_info_id"])
+        # courseID X SID dictionary  上這門課的學生有哪些
+        CID_SID = {}
+        for i in CID_URL.keys():
+            CID_SID[i] = []
+            cids_sid = list(Tuition.objects.filter(course_schedule_id=i).values())
+            for j in range(len(cids_sid)):
+                CID_SID[i].append(cids_sid[j]["student_info_id"])
 
-    # 今天有簽到的同學們
-    today_arrive = list(Attendance.objects.filter(
-        in_out="in", date_time__gt=datetime.date(2020, 5, 17)).values())  # 這邊應該要是today
-    today_arr_SID = []
-    for i in range(len(today_arrive)):
-        today_arr_SID.append(today_arrive[i]["student_info_id"])
+        # 今天有簽到的同學們
+        today_arrive = list(Attendance.objects.filter(
+            in_out="in", date_time__gt=datetime.date(2020, 5, 17)).values())  # 這邊應該要是today
+        today_arr_SID = []
+        for i in range(len(today_arrive)):
+            today_arr_SID.append(today_arrive[i]["student_info_id"])
 
-    #SID (課的ID,課的URL)
-    SID_URL = {}
-    for i in CID_SID.keys():
-        for j in CID_SID[i]:
-            if j not in today_arr_SID:
-                if j not in SID_URL:
-                    SID_URL[j] = []
-                    SID_URL[j].append((i, CID_URL[i]))
-                else:
-                    SID_URL[j].append((i, CID_URL[i]))
+        #SID (課的ID,課的URL)
+        SID_URL = {}
+        for i in CID_SID.keys():
+            for j in CID_SID[i]:
+                if j not in today_arr_SID:
+                    if j not in SID_URL:
+                        SID_URL[j] = []
+                        SID_URL[j].append((i, CID_URL[i]))
+                    else:
+                        SID_URL[j].append((i, CID_URL[i]))
 
-    send_to = []
-    for i in SID_URL.keys():
-        that_stu_email = list(StudentInfo.objects.filter(id=i).values())[
-            0]["email"]
-        that_stu_name = list(StudentInfo.objects.filter(id=i).values())[
-            0]["student_name"]
-        send_class = ""
-        for j in range(len(SID_URL[i])):
-            real_courseID = list(CourseSchedule.objects.filter(
-                id=SID_URL[i][j][0]).values())[0]["courseID"]
+        send_to = []
+        for i in SID_URL.keys():
+            that_stu_email = list(StudentInfo.objects.filter(id=i).values())[
+                0]["email"]
+            that_stu_name = list(StudentInfo.objects.filter(id=i).values())[
+                0]["student_name"]
+            send_class = ""
+            for j in range(len(SID_URL[i])):
+                real_courseID = list(CourseSchedule.objects.filter(
+                    id=SID_URL[i][j][0]).values())[0]["courseID"]
 
-            gmail_user = 'superandy0407@gmail.com'
-            gmail_password = 'superandy'  # your gmail password
+                gmail_user = 'superandy0407@gmail.com'
+                gmail_password = 'superandy'  # your gmail password
 
-            msg = MIMEText(that_stu_name+'同學你好!\n今天你缺席了(課程ID:' +
-                           real_courseID+')這門課\n補課影片連結：'+SID_URL[i][j][1]+'\n請及時補完^^')
-            msg['Subject'] = '孔銘數學家教班補課連結'
-            msg['From'] = gmail_user
-            msg['To'] = that_stu_email
+                #msg = MIMEText(that_stu_name+'同學你好!\n今天你缺席了(課程ID:' +
+                #               real_courseID+')這門課\n補課影片連結：'+SID_URL[i][j][1]+'\n請及時補完^^')
+                #msg['Subject'] = '孔銘數學家教班補課連結'
+                #msg['From'] = gmail_user
+                #msg['To'] = that_stu_email
 
-            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            server.ehlo()
-            server.login(gmail_user, gmail_password)
-            server.send_message(msg)
-            server.quit()
-            send_class = send_class + " " + real_courseID
+                #server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                #server.ehlo()
+                #server.login(gmail_user, gmail_password)
+                #server.send_message(msg)
+                #server.quit()
+                send_class = send_class + " " + real_courseID
 
-#             print('Email sent!')
-        send_to.append(that_stu_name + send_class + " " + that_stu_email)
-    print(send_to)
-    data = {"show": send_to}
-    return render(request, 'sentEmail.html', data)
+    #             print('Email sent!')
+            send_to.append(that_stu_name + send_class + " " + that_stu_email)
+        print(send_to)
+        send_to = json.dumps(send_to)
+        return HttpResponse(send_to)
+    else:
+        print(request.body.decode('ascii').split(","))
+        # Modify the real database
+        return HttpResponse(request)        
 
 
 @csrf_exempt
 def GetScholar(request):
     if request.method == "GET":
-
         school_record = SchoolRecord.objects.all()
         recipient = dict()
         for sr in school_record:
@@ -145,6 +149,7 @@ def GetScholar(request):
                             "amount" : int(receiver_.scholarship_payment)
                         }
                         recipient.setdefault(receiver_["name"], receiver_)
+
         print(recipient)
         recipient = json.dumps(recipient)
         return HttpResponse(recipient)
